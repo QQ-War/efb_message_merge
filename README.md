8858f6d6c406034d61262486691a1d9c7b3a6a71
hizhongcai <hizhongcai@hotmail.com>
2022-08-24 03:00:30 +0000

    增加整合相同信息发送人到一条信息的功能

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 当前实现的功能：
 ```
 1.实现主端发送的消息，如果从端会再以Sendoutmessage的形式发回来，屏蔽掉。
+2.对于发送的相同消息，整合并显示该成员发送的次数。相同消息在配置文件里面配置。(参考https://github.com/iaurman/efb-msg-filter的代码）
 ```
 
 ## 1.安装
@@ -18,4 +19,11 @@ middlewares:
 - 其他middleware
 - QQ_War.message_merge
 ```
+## 3.配置文件config.yaml，放到QQ_War.message_merge目录下面
+```
+mastersendback: True
 
+samemessage:
+ - "收到/取消 群语音邀请"
+ - "收到"
+```
--- efb_message_merge/__init__.py
+++ efb_message_merge/__init__.py
@@ -7,7 +7,7 @@ from typing import Dict , Any
 
 from ehforwarderbot import Middleware, Message, Status, coordinator, utils, MsgType
 from ehforwarderbot.types import ModuleID, MessageID, InstanceID, ChatID
-from ehforwarderbot.chat import Chat, SystemChat, GroupChat
+from ehforwarderbot.chat import Chat, SystemChat, GroupChat, ChatNotificationState
 from ehforwarderbot.message import MsgType, MessageCommands, MessageCommand, Substitutions
 from ehforwarderbot import utils as efb_utils
 
@@ -22,45 +22,124 @@ class MessageMergeMiddleware(Middleware):
     
     def __init__(self, instance_id: Optional[InstanceID] = None):
         super().__init__(instance_id)
-        #config_path = efb_utils.get_config_path(self.middleware_id)
-        #self.config = self.load_config(config_path)
-        #self.keywords=self.config['keywords'] if 'keywords' in self.config.keys() else {}
-        self.messagecache = list()
+        config_path = efb_utils.get_config_path(self.middleware_id)
+        self.config = self.load_config(config_path)
+        self.mastersendback = self.config.get("mastersendback", True)
+        self.samemessageconfig = self.config.get('samemessage', [])
+
+        self.mastersendoutmessagecache = list()
+        """ 
+            [message1, message2,...]
+        """
+        self.smmcache = {}
+        """
+            [samemessage:{
+                "[group_id]":
+                    {
+                        time: "1234567890",
+                        members: {name1:1, name2:3}
+                        uid: "8908ds_sadjd_3dkajs"
+                    }
+                }
+            ]
+        """
+        for i in self.samemessageconfig:
+            self.smmcache[i] = {}
 
 
-    #@staticmethod
-    #def load_config(path : str) -> Dict[str, Any]:
-    #    if not path.exists():
-    #        return
-    #    with path.open() as f:
-    #        d = yaml.full_load(f)
-    #        if not d:
-    #            return
-    #        config: Dict[str, Any] = d
-    #    return config
+    @staticmethod
+    def load_config(path : str) -> Dict[str, Any]:
+        if not path.exists():
+            return
+        with path.open() as f:
+            d = yaml.full_load(f)
+            if not d:
+                return
+            config: Dict[str, Any] = d
+        return config
 
     @staticmethod
     def sent_by_master(message: Message) -> bool:
         return message.deliver_to != coordinator.master
 
     def process_message(self, message: Message) -> Optional[Message]:
-        if message.type == MsgType.Text:
-            return self.mergetextmessage(message)
+        if message.type == MsgType.Text and self.mastersendback:
+            message = self.mergemastersendouttextmessage(message)
+        
+        for i in self.samemessageconfig:
+            if message.text == i:
+                message = self.mergesamemessage(message, i)
+
         return message
         
 
-    def mergetextmessage(self, message: Message) -> Optional[Message]:
+    def mergemastersendouttextmessage(self, message: Message) -> Optional[Message]:
         if self.sent_by_master(message):
-            self.messagecache.append(message)
-            if len(self.messagecache)>30:
-                self.messagecache.pop(0)
+            self.mastersendoutmessagecache.append(message)
+            if len(self.mastersendoutmessagecache)>30:
+                self.mastersendoutmessagecache.pop(0)
             return message
         else:
-            for i in self.messagecache:
+            for i in self.mastersendoutmessagecache:
                 if i.text == message.text and i.chat.uid == message.chat.uid :
-                    self.messagecache.remove(i)
+                    self.mastersendoutmessagecache.remove(i)
                     #message.edit = True
                     #message.uid = i.uid
                     #message.deliver_to = coordinator.master
                     return None 
         return message
+
+    def mergesamemessage(self, message: Message, samemessage: str):
+        def get_name():
+            if message.author.alias == '' or message.author.alias is None:
+                return message.author.name
+            else:
+                return message.author.alias
+
+        name=get_name()
+
+        def implement():
+            message.text = samemessage+'\n' + name+"*1"
+            self.smmcache[samemessage][message.chat.uid] = {
+                'time': time.time(),
+                'members': {name:1},
+                'uid': message.uid
+            }
+            message.author = sys_author
+            #message.chat.notification = ChatNotificationState.ALL
+            return message
+
+        sys_author = message.chat.make_system_member(
+            uid="QQ_War.message_merge",
+            name="msg_merge",
+            middleware=self
+        )
+
+
+        if message.chat.uid not in self.smmcache[samemessage]:
+            return implement()
+        else:
+            # The original samemessage should be valid only within 2 hours
+            if time.time() - self.smmcache[samemessage][message.chat.uid]["time"] < 7200.0:
+                # Update Msg
+                if name in self.smmcache[samemessage][message.chat.uid]["members"].keys():
+                    self.smmcache[samemessage][message.chat.uid]["members"][name]+=1
+                else:
+                    self.smmcache[samemessage][message.chat.uid]["members"][name]=1
+                
+                if not self.sent_by_master(message):
+                    message.text = samemessage + "\n"
+                    for i in self.smmcache[samemessage][message.chat.uid]["members"]:
+                        message.text += i + "*" + str(self.smmcache[samemessage][message.chat.uid]["members"][i]) +", "
+                    message.text.strip(", ")
+                    message.edit = True
+                    message.uid = MessageID(self.smmcache[samemessage][message.chat.uid]["uid"])
+                    message.author = sys_author
+                    message.type = MsgType.Text
+
+                #message.chat.notification = ChatNotificationState.ALL
+
+                return message
+            # If not, implement a new one
+            else:
+                return implement()
