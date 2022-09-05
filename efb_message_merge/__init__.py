6b581afcf4b93db63d1a7c186e98075b919cac5c
hizhongcai <hizhongcai@hotmail.com>
2022-09-05 23:00:13 +0000

    增加debug日志，同时修复私聊信息merge的bug

--- efb_message_merge/__init__.py
+++ efb_message_merge/__init__.py
@@ -1,4 +1,5 @@
 import threading
+import logging
 import time
 import yaml
 import re
@@ -18,10 +19,14 @@ class MessageMergeMiddleware(Middleware):
     middleware_id: ModuleID = ModuleID("QQ_War.message_merge")
     middleware_name: str = "Message Merge Middleware"
     __version__: str = '0.1.0'
+    logger: logging.Logger = logging.getLogger(
+        "plugins.%s.MessageMergeMiddleware" % middleware_id)
 
     
     def __init__(self, instance_id: Optional[InstanceID] = None):
+        
         super().__init__(instance_id)
+
         config_path = efb_utils.get_config_path(self.middleware_id)
         self.config = self.load_config(config_path)
         self.mastersendback = self.config.get("mastersendback", True)
@@ -89,12 +94,14 @@ class MessageMergeMiddleware(Middleware):
     def mergemastersendouttextmessage(self, message: Message) -> Optional[Message]:
         if self.sent_by_master(message):
             self.mastersendoutmessagecache.append(message)
+            self.logger.debug('master send message:'+str(message))
             if len(self.mastersendoutmessagecache)>30:
                 self.mastersendoutmessagecache.pop(0)
             return message
         else:
             for i in self.mastersendoutmessagecache:
                 if i.text == message.text and i.chat.uid == message.chat.uid :
+                    self.logger.debug('slave send back master message:'+str(message))
                     self.mastersendoutmessagecache.remove(i)
                     #message.edit = True
                     #message.uid = i.uid
@@ -120,13 +127,14 @@ class MessageMergeMiddleware(Middleware):
             smmcache[samemessage][message.chat.uid] = {
                 'time': time.time(),
                 'members': {name:1},
-                'uid': message.uid
+                'uid': message.uid+'QQ_War.message_merge'
             }
             #message.author = sys_author
 
-            if self.sent_by_master(message):
+            #if self.sent_by_master(message):
                 #message.author = message.chat.self
-                smmcache[samemessage][message.chat.uid]['uid'] = "{uni_id}".format(uni_id=str(int(time.time())))
+            #    smmcache[samemessage][message.chat.uid]['uid'] = "{uni_id}".format(uni_id=str(int(time.time())))
+            message.uid += 'QQ_War.message_merge'
             return message
 
         sys_author = message.chat.make_system_member(
@@ -137,6 +145,7 @@ class MessageMergeMiddleware(Middleware):
 
 
         if message.chat.uid not in smmcache[samemessage]:
+            self.logger.debug('first message to be merged:'+str(message))
             return implement()
         else:
             # The last samemessage should be valid only within 10 minutes
@@ -161,7 +170,11 @@ class MessageMergeMiddleware(Middleware):
 
                 #message.chat.notification = ChatNotificationState.ALL
 
+                self.logger.debug('messages merged:'+str(message))
                 return message
             # If not, implement a new one
             else:
+                self.logger.debug('messages to be merged start over:'+str(message))
                 return implement()
+
+        return message
